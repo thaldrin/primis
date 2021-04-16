f2873e1c974446eb561468fde5c3560fa8494a57
Lio Young <lio@werewolf.design>
2021-04-16 15:45:42 +0200

    add cache

---
+++ utils/cache.ts
@@ -0,0 +1,36 @@
+import nodecache from 'node-cache'
+import supabase from "@utils/database";
+import routes from "@utils/routes";
+import chalk from 'chalk'
+import { Image } from "@utils/api.types";
+
+const cache = new nodecache({
+    stdTTL: 60
+})
+// @ts-ignore
+cache.on("set", (key, value) => {
+
+    console.log(`Updated /${chalk.red(key)} with latest data`)
+
+})
+
+function update_cache(data: any, route: string) {
+    cache.set(route, data)
+}
+
+export async function getdata() {
+    routes.forEach(async (route) => {
+        let { data } = await supabase.from<Image>('images').select().eq("folder", route)
+
+        update_cache(data, route)
+    })
+
+}
+
+
+getdata()
+setInterval(async () => {
+    await getdata()
+}, 60 * 1000)
+
+export default cache
